cc9954d54dd4d125fa2ca43a6c1ee0e062a9f2cf
Erick Lopez <contactme@ericklopez.dev>
2024-10-19 11:50:50 -0500

    fixed responsiveness for mobile

--- calorie-app/src/app/sign-in/page.js
+++ calorie-app/src/app/sign-in/page.js
@@ -20,15 +20,15 @@ const SignInPage = () => {
 
   return (
     <>
-      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex ">
+      <div className="container relative h-screen flex-col items-center justify-center md:grid md:max-w-none md:grid-cols-2 lg:px-0">
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white md:flex lg:flex ">
           <div className="absolute inset-0 bg-black" />
           <div className="relative z-20 flex items-center text-lg font-medium text-white">
             Oklathon Calorie Tracker
           </div>
         </div>
-        <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="p-2 mt-48 sm:mt-0 lg:p-8 w-screen md:w-full mx-auto">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
                 Sign into your account
@@ -37,7 +37,7 @@ const SignInPage = () => {
                 Enter your details below to sign into your account
               </p>
             </div>
-            <div className={"grid gap-6"}>
+            <div className="grid gap-6 px-12">
               <form onSubmit={handleSubmit}>
                 <div className="grid gap-2">
                   <div className="grid gap-1">
