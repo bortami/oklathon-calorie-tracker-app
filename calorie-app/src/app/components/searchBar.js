7df6bfc3acc30b94208eaad01ce4d78278222fc5
Kris Norwood <kristofernorwood@gmail.com>
2024-10-19 15:23:41 -0500

    Adding clarifying comments; clearing out some currently unused code

--- calorie-app/src/app/components/searchBar.js
+++ calorie-app/src/app/components/searchBar.js
@@ -4,7 +4,9 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 
 export function SearchBar() {
+    // Food item requested
     const [foodItem, setFoodItem] = useState('');
+    // Array of items requested
     const [foodList, setFoodList] = useState([]);
 
     const handleSubmit = (e) => {
@@ -13,7 +15,6 @@ export function SearchBar() {
         const axios = require('axios');
 
         const apiKey = 'RN2x+V3YhTirpJM0ExxClQ==e1GElmIAUMseOMO5';
-        // const query = '12oz onion and a tomato';
 
         axios.get('https://api.calorieninjas.com/v1/nutrition?query=' + foodItem, {
             headers: {
@@ -21,46 +22,17 @@ export function SearchBar() {
             }
         })
             .then(response => {
-                let allFoodList = foodList;
+                let allFoodList = foodList; // Temporary array for storing food items
                 const obj = response.data;
-                // console.log("Object keys are: ", Object.keys(obj));
-                // console.log(Object.keys(obj));
-                // console.log(obj.items[0]);
-                // console.log(obj.items[0]['name']);
-                // console.log(obj.items[0]['serving_size_g']);
-                // console.log(obj.items[0]['fat_total_g']);
-                // console.log(obj.items[0]['fat_saturated_g']);
-                // console.log(obj.items[0]['protein_g']);
-                // console.log(obj.items[0]['sodium_mg']);
-                // console.log(obj.items[0]['potassium_mg']);
-                // console.log(obj.items[0]['cholesterol_mg']);
-                // console.log(obj.items[0]['carbohydrates_total_g']);
-                // console.log(obj.items[0]['fiber_g']);
-                // console.log(obj.items[0]['sugar_g']);
+                
+                // Iterate over response object array, push to temp array
                 obj.items.forEach(item => {
                     console.log("Food item name is: ", item.name);
                     allFoodList.push(item)
                 });
+                // Update foodList state variable with existing values, append temporary array values
                 setFoodList([...foodList, allFoodList]);
                 console.log("Food list contains:", foodList);
-                // setFoodList(allFoodList);
-                // let totalCalories = 0.0, totalFat = 0.0, totalSaturatedFat = 0.0, totalProtein = 0.0, totalSodium = 0.0, totalPotassium = 0.0, totalCholesterol = 0.0, totalCarbohydrates = 0.0, totalFiber = 0.0, totalSugar = 0.0;
-                // for (let i = 0; i < obj.items.length; i++) {
-                //     console.log('name: ', obj.item[i]['name'])
-                //     totalCalories = totalCalories + obj.items[i]['calories'];
-                //     totalFat = totalFat + obj.items[i]['fat_total_g'];
-                //     totalSaturatedFat = totalSaturatedFat + obj.items[i]['fat_saturated_g']
-                //     totalProtein = totalProtein + obj.items[i]['protein_g'];
-                //     totalSodium = totalSodium + obj.items[i]['sodium_mg'];
-                //     totalPotassium = totalPotassium + obj.items[i]['potassium_mg'];
-                //     totalCholesterol = totalCholesterol + obj.items[i]['cholesterol_mg'];
-                //     totalCarbohydrates = totalCarbohydrates + obj.items[i]['carbohydrates_total_g'];
-                //     totalFiber = totalFiber + obj.items[i]['fiber_g'];
-                //     console.log('the total grams of sugar is:', obj.items[i]['sugar_g']);
-                //     totalSugar = totalSugar + obj.items[i]['sugar_g'];
-                //     console.log('\n');
-                // }
-
             })
             .catch(error => {
                 console.log('Errored')
