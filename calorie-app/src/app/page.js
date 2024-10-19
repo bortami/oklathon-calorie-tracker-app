555888df08e67214b9246374af6f6ee29646c69c
Erick Lopez <contactme@ericklopez.dev>
2024-10-19 14:05:47 -0500

    Added calorie chart and food table to dashboard

--- calorie-app/src/app/page.js
+++ calorie-app/src/app/page.js
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import { RadialChart } from "./components/calorieChart";
+import { FoodTable } from "./components/foodTable";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="items-center justify-items-center justify-center w-screen min-h-screen p-4 pb-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col row-start-2 justify-items-center items-center justify-center">
+        <div className="w-64">
+          <RadialChart></RadialChart>
+          <FoodTable></FoodTable>
+        </div>
       </main>
     </div>
   );
